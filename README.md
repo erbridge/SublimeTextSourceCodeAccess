8fd133be396212f0196a8f68fed037cb0f36a92a
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-03-27 00:53:17 +0000

    Initial commit.
    Code modified from https://github.com/fire/SensibleEditorSourceCodeAccess/tree/0947a85b555dd853d0c7ecabb25e87b00d9e7a0d.

---
+++ README.md
@@ -0,0 +1,5 @@
+# SublimeTextSourceCodeAccess
+
+Based on code from [fire/SensibleEditorSourceCodeAccess](https://github.com/fire/SensibleEditorSourceCodeAccess).
+
+Clone this to your UnrealEngine directory at `Engine/Plugins/Developer` (symlinking doesn't work).
---
+++ Source/SublimeTextSourceCodeAccess/Private/SublimeTextSourceCodeAccessor.cpp
@@ -0,0 +1,147 @@
+ /* Copyright (c) 2014 K. Ernest 'iFire' Lee
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+#include "SublimeTextSourceCodeAccessPrivatePCH.h"
+#include "SublimeTextSourceCodeAccessor.h"
+#include "DesktopPlatformModule.h"
+
+#define LOCTEXT_NAMESPACE "SublimeTextSourceCodeAccessor"
+
+bool FSublimeTextSourceCodeAccessor::CanAccessSourceCode() const
+{
+    return FPaths::FileExists(TEXT("/usr/bin/clang"));
+}
+
+FName FSublimeTextSourceCodeAccessor::GetFName() const
+{
+    return FName("SublimeTextSourceCodeAccessor");
+}
+
+FText FSublimeTextSourceCodeAccessor::GetNameText() const
+{
+    return LOCTEXT("SublimeTextDisplayName", "Sublime Text");
+}
+
+FText FSublimeTextSourceCodeAccessor::GetDescriptionText() const
+{
+    return LOCTEXT("SublimeTextDisplayDesc", "Open source code files with Sublime Text");
+}
+
+bool FSublimeTextSourceCodeAccessor::OpenSolution()
+{
+    FString FullPath;
+    if ( FDesktopPlatformModule::Get()->GetSolutionPath(FullPath) )
+    {
+        if ( FPaths::FileExists(FullPath) )
+        {
+            // Add this to handle spaces in path names.
+            const FString NewFullPath = FString::Printf(TEXT("\"%s\""), *FullPath);
+
+            FString Editor = FString(TEXT("/usr/bin/subl"));
+            if ( FLinuxPlatformProcess::CreateProc(
+                    *Editor,
+                    *NewFullPath,
+                    true,
+                    true,
+                    false,
+                    nullptr,
+                    0,
+                    nullptr,
+                    nullptr
+                ).IsValid() )
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool FSublimeTextSourceCodeAccessor::OpenFileAtLine(const FString& FullPath, int32 LineNumber, int32 ColumnNumber)
+{
+    FString Editor = FString(TEXT("/usr/bin/subl"));
+
+    // Add this to handle spaces in path names.
+    const FString NewFullPath = FString::Printf(TEXT("\"%s:%d:%d\""), *FullPath, LineNumber, ColumnNumber);
+
+    if ( FLinuxPlatformProcess::CreateProc(
+            *Editor,
+            *NewFullPath,
+            true,
+            true,
+            false,
+            nullptr,
+            0,
+            nullptr,
+            nullptr
+        ).IsValid() )
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool FSublimeTextSourceCodeAccessor::OpenSourceFiles(const TArray<FString>& AbsoluteSourcePaths)
+{
+    for ( const FString& SourcePath : AbsoluteSourcePaths )
+    {
+        FString Editor = FString(TEXT("/usr/bin/subl"));
+
+        // Add this to handle spaces in path names.
+        const FString NewSourcePath = FString::Printf(TEXT("\"%s\""), *SourcePath);
+
+        if ( !(FLinuxPlatformProcess::CreateProc(
+                *Editor,
+                *NewSourcePath,
+                true,
+                true,
+                false,
+                nullptr,
+                0,
+                nullptr,
+                nullptr
+            ).IsValid()) )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool FSublimeTextSourceCodeAccessor::AddSourceFiles(const TArray<FString>& AbsoluteSourcePaths, const TArray<FString>& AvailableModules)
+{
+    return false;
+}
+
+bool FSublimeTextSourceCodeAccessor::SaveAllOpenDocuments() const
+{
+    return false;
+}
+
+void FSublimeTextSourceCodeAccessor::Tick(const float DeltaTime)
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
